547a731da713190c8ada8c36e5456771d2b5005b
Felipe <felipebalderas91@gmail.com>
2018-11-26 18:11:34 -0600

    Remove App.js, add Row2

--- empresa1/src/index.js
+++ empresa1/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import Row1 from './rows/row1.js';
+import Row2 from './rows/row2.js';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<Row1 />, document.getElementById('row1'));
+ReactDOM.render(<Row2 />, document.getElementById('row2'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
